3e503b83a08d748e34ccc8d9599ea00f73c0fae8
Mredul <mredulsaha1000@gmail.com>
2025-11-11 14:26:21 +0600

    send data database to ui

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
 
 
 // middleware----
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://SebajatradbUser:yW9FH8PTVBnlCC3G@mycluster.sc65jvq.mongodb.net/?appName=myCluster";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mycluster.sc65jvq.mongodb.net/?appName=myCluster`;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -28,10 +28,20 @@ app.get('/', (req, res) => {
 async function run() {
     try {
         await client.connect();
+        const sebajatraDB = client.db('sebajatra_db');
+        const upcomingEvents = sebajatraDB.collection('upcomingEvents')
+
+        app.get('/upcoming-events', async (req, res) => {
+            const cursor = upcomingEvents.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
